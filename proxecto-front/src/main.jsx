d81586dc47d4850a7ea90875a5fba8fba64b6922
Andres Hermelo <andres.hermelo@gmail.com>
2025-11-17 20:46:53 +0100

    Modify App.jsx and set main.jsx

--- proxecto-front/src/main.jsx
+++ proxecto-front/src/main.jsx
@@ -1,10 +1,29 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
+import {provider} from 'react-redux'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import {store} from 'app/store'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const container = document.getElementById('root')
+
+if (container) {
+  const root = createRoot(container);
+
+  const router = createBrowserRouter([
+    { path: '/', element: <App /> }
+  ]);
+
+  root.render(
+    <React.StrictMode>
+      <provider store={store}>
+        <RouterProvider router={router} />
+      </provider>
+    </React.StrictMode>
+  );
+}else {
+  throw new Error(
+    'Root element with ID "root" not found in the HTML document.' 
+  );
+}
